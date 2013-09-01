f2fff85bb7184c2865306ea088d202f37e8336b6
Jonny <myself@jonny.li>
2013-08-31 18:39:40 -0700

    Update jkscrolling.js

--- jkscrolling.js
+++ jkscrolling.js
@@ -1,9 +1,8 @@
 var alignHeight = getVerticalPos(getAlign());
 var posts = getAllPosts();
-var current = posts[0];
+var current;
 window.onkeypress = function(e) {
 	if (posts.length <= 1) return;
-	if (typeof(current) === "undefined") return;
 	alignHeight = getVerticalPos(getAlign());
 	var speed = 100;
 	switch (e.keyCode) {
@@ -15,16 +14,6 @@ window.onkeypress = function(e) {
 			break;
 	}
 };
-function scrollDown(speed) {
-	var newPost = current.nextElementSibling;
-	if (newPost.className == "tumblrAutoPager_page_info") newPost = newPost.nextElementSibling; // compatibility with endless scrolling
-	if (newPost != null && newPost.dataset.jks == "post") scrollTo(document.body, newPost.offsetTop - alignHeight, speed);
-}
-function scrollUp(speed) {
-	var newPost = current.previousElementSibling;
-	if (newPost.className == "tumblrAutoPager_page_info") newPost = newPost.previousElementSibling; // compatibility with endless scrolling
-	if (newPost != null && newPost.dataset.jks == "post") scrollTo(document.body, newPost.offsetTop - alignHeight, speed);
-}
 window.onscroll = function() {
 	alignHeight = getVerticalPos(getAlign());
 	var fromTop = getScrollTop() + alignHeight;
@@ -39,6 +28,17 @@ window.onscroll = function() {
 		}
 	}
 };
+function scrollDown(speed) {
+	var newPost = typeof(current) === "undefined" ? posts[0] : current.nextElementSibling;
+	if (newPost.className == "tumblrAutoPager_page_info") newPost = newPost.nextElementSibling; // compatibility with endless scrolling
+	if (newPost != null && newPost.dataset.jks == "post") scrollTo(document.body, newPost.offsetTop - alignHeight, speed);
+}
+function scrollUp(speed) {
+	if (typeof(current) === "undefined") return;
+	var newPost = getVerticalPos(current) < alignHeight ? current : current.previousElementSibling;
+	if (newPost.className == "tumblrAutoPager_page_info") newPost = newPost.previousElementSibling; // compatibility with endless scrolling
+	if (newPost != null && newPost.dataset.jks == "post") scrollTo(document.body, newPost.offsetTop - alignHeight, speed);
+}
 function scrollTo(element, to, duration) {
 	if (duration <= 0) return;
 	var start = element.scrollTop,
@@ -53,17 +53,6 @@ function scrollTo(element, to, duration) {
 	};
 	animateScroll();
 }
-//t = current time
-//b = start value
-//c = change in value
-//d = duration
-Math.easeInOutQuad = function (t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t + b;
-	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
-};
-
 function getScrollTop() {
 	if (typeof pageYOffset != 'undefined') {
 		//most browsers except IE before #9
@@ -81,3 +70,14 @@ function getVerticalPos(node) { // get vertical position from window top
 }
 function getAlign() { return document.querySelector("[data-jks=\"align\"]"); }
 function getAllPosts() { return document.querySelectorAll("[data-jks=\"post\"]"); }
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
